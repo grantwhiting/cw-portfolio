4af8ccf8684e315411b96d885f805b20d84c8814
Grant Whiting <grant.whiting@daveramsey.com>
2021-08-26 20:49:03 -0500

    add initial site setup

---
+++ functions.php
@@ -0,0 +1,42 @@
+<?php
+  add_filter( 'register_post_type_args', function( $args, $post_type ) {
+
+    if ( 'project' === $post_type ) {
+      $args['show_in_graphql'] = true;
+      $args['graphql_single_name'] = 'project';
+      $args['graphql_plural_name'] = 'projects';
+    }
+
+    return $args;
+
+  }, 10, 2 );
+
+  add_action( 'graphql_register_types', function() {
+
+    register_graphql_object_type(
+      'GalleryImage',
+      [
+        'description' => __( 'Gallery Image' ),
+        'fields'  => [
+          'guid'  => [
+            'type'  => 'String',
+            'description' => 'image guid'
+          ]
+        ],
+      ]
+    );
+
+    register_graphql_field(
+      'Project',
+      'galleryImages',
+      [
+        'description' => __( 'Return images' ),
+        'type' => [ 'list_of' => 'galleryImage' ],
+        'resolve' => function() {
+          $images = pods('project', [ 'limit' => -1 ])->field( 'gallery' );
+          return empty( $images ) ? [] : $images;
+        }
+      ]
+    );
+  } );
+?>
