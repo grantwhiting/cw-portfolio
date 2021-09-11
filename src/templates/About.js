e2760923d9ffa91612ff11e6f32fa701d87576ac
Grant Whiting <grant.whiting@daveramsey.com>
2021-09-11 12:48:22 -0500

    fix navigation to 'about' page

--- src/templates/About.js
+++ src/templates/About.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { PropTypes } from "prop-types";
+import { useLocation } from "@reach/router";
 import Layout from "../components/layout";
 
 const About = ({ pageContext }) => {
   console.log(pageContext);
+  const location = useLocation();
   const { content } = pageContext;
 
   return (
-    <Layout>
+    <Layout location={location}>
       <div dangerouslySetInnerHTML={{ __html: content }}></div>
     </Layout>
   );
