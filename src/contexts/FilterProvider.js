f88c1523c19164a5d31ba6a3d8aa6a813e73d54b
Grant Whiting <grant.whiting@daveramsey.com>
2021-09-14 20:32:34 -0500

    create filter context provider

---
+++ src/contexts/FilterProvider.js
@@ -0,0 +1,30 @@
+import React, { useContext, createContext } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+import PropTypes from "prop-types";
+
+const FilterContext = createContext({});
+
+export const useFilter = useContext(FilterContext);
+
+const FilterProvider = ({ children }) => {
+  const [storedValue, setLocalStorageValue] = useLocalStorage(
+    "currentFilter",
+    "all"
+  );
+
+  return (
+    <FilterContext.Provider
+      value={{
+        currentFilter: storedValue,
+        setCurrentFilter: setLocalStorageValue,
+      }}
+    >
+      {children}
+    </FilterContext.Provider>
+  );
+};
+
+FilterProvider.propTypes = PropTypes.oneOfType([
+  PropTypes.arrayOf(PropTypes.node),
+  PropTypes.node,
+]).isRequired;
