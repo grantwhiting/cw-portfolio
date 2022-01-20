66a5d756f8bc9cf24d0562cee362f92dd9db8980
Grant Whiting <grant.whiting@daveramsey.com>
2022-01-19 21:26:34 -0600

    animate about

--- src/templates/aboutPage.js
+++ src/templates/aboutPage.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { PropTypes } from "prop-types";
 import Layout from "../components/layout";
+import { motion } from "framer-motion";
 
 const AboutPage = ({ pageContext }) => {
   const { content } = pageContext;
 
   return (
     <Layout>
-      <div dangerouslySetInnerHTML={{ __html: content }}></div>
+      <motion.div
+        dangerouslySetInnerHTML={{ __html: content }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+      ></motion.div>
     </Layout>
   );
 };
