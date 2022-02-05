3ef919ee00478735845cb6417a3b312ddb32e09f
Grant Whiting <grant.whiting@daveramsey.com>
2022-02-05 15:49:42 -0600

    about page styles

--- src/templates/aboutPage.js
+++ src/templates/aboutPage.js
@@ -2,17 +2,28 @@ import React from "react";
 import { PropTypes } from "prop-types";
 import Layout from "../components/layout";
 import { motion } from "framer-motion";
+import { StaticImage } from "gatsby-plugin-image";
 
 const AboutPage = ({ pageContext }) => {
   const { content } = pageContext;
 
   return (
     <Layout>
-      <motion.div
-        dangerouslySetInnerHTML={{ __html: content }}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-      ></motion.div>
+      <div className="flex flex-col lg:flex-row">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          className="flex self-center flex-1 lg:order-1"
+        >
+          <StaticImage src="../images/About-Pic.png" alt="Courtney Whiting" />
+        </motion.div>
+        <motion.div
+          dangerouslySetInnerHTML={{ __html: content }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          className="flex-1 lg:mr-6"
+        ></motion.div>
+      </div>
     </Layout>
   );
 };
