9a3b11718458d6948f4bb9a66c7168a4ee9f610a
grant.whiting <whiting.grant@gmail.com>
2023-10-02 21:15:11 -0500

    add netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-transformer-sharp",
     "gatsby-plugin-client-side-redirect",
+    "gatsby-plugin-netlify",
     {
       resolve: "gatsby-plugin-react-svg",
       options: {
